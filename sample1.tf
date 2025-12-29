d36583bfd2dec92e71962733b5005163bfb5ca12
Jay <jay951054@gmail.com>
2025-12-29 17:10:14 +0530

    variable and output block added

--- sample1.tf
+++ sample1.tf
@@ -1,11 +1,36 @@
+//provider Block
 provider "aws" {
     region = "ap-south-1"
   
 }
-
+//resource Block
 resource "aws_instance" "terafform" {
-    ami = "ami-02b8269d5e85954ef"
-    instance_type = "t3.micro"
-    key_name = "Demo"
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
     vpc_security_group_ids = ["sg-08de88a7810b42a8f"]  
-}       
+    availability_zone = "ap-east-1a"
+    tags = {
+      name= "my instance"
+    }
+}   
+//variables Block
+variable "ami_id" {  
+  default = "ami-02b8269d5e85954ef"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "key_name" {
+    default = "Demo"
+  
+}
+
+//outputs Block
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.ec2_instance.public_ip
+}
